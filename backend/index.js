6a3b9b5b014834f272b01e122f674d8ec7c29c25
Rayk <raykrocha1@gmail.com>
2020-03-28 01:27:46 -0300

    setting database

--- backend/index.js
+++ backend/index.js
@@ -1,4 +1,14 @@
 const express = require('express');
 const app = express();
 
+app.use(express.json());
+
+app.post('/users', (request, response) => {
+    const params = request.body;
+
+    console.log(params);
+    return response.json({ 
+        teste: 'Olá Mundo' 
+    });
+})
 app.listen(3333);
