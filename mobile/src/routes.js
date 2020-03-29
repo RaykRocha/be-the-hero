f128787fa687db2acae64950a30882611b5084df
Rayk <raykrocha1@gmail.com>
2020-03-29 01:56:21 -0300

    styling index.js incidents

--- mobile/src/routes.js
+++ mobile/src/routes.js
@@ -1,16 +1,19 @@
-import { NavigationContainer } from '@react-navigation/stack';
-import { createStackNavigator } from '@react-navigation/native';
+import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
 
-const AppStack = createStackNavigator();
 import Incidents from './pages/Incidents';
 import Detail from './pages/Detail';
+
+const AppStack = createStackNavigator();
+
 export default function Routes() {
-    return (
-        <NavigationContainer>
-            <AppStack.Navigation>
-                <AppStack.Screen componet={Incidents} />
-                <AppStack.Screen componet={Detail} />
-            </AppStack.Navigation>
-        </NavigationContainer>
-    );
+  return (
+    <NavigationContainer>
+      <AppStack.Navigator screenOptions={{ headerShown: false }}>
+        <AppStack.Screen name="Incidents" component={Incidents} />
+        <AppStack.Screen name="Detail" component={Detail} />
+      </AppStack.Navigator>
+    </NavigationContainer>
+  );
 }
