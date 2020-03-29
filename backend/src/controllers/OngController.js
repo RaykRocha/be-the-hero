4d3ee70615388385887e359508c44d06d5781aa6
Rayk <raykrocha1@gmail.com>
2020-03-29 19:58:16 -0300

    fazendo teste com jest

--- backend/src/controllers/OngController.js
+++ backend/src/controllers/OngController.js
@@ -1,5 +1,5 @@
-const crypto = require('crypto');
 const connection = require('../database/connection');
+const generateUniqueID = require('../utils/generateUniqueId');
 
 module.exports = {
     async index(request, response) {
@@ -8,7 +8,7 @@ module.exports = {
     },
     async create(request, response) {
         const { name, email, whatsapp, city, uf } = request.body;
-        const id = crypto.randomBytes(4).toString('HEX');
+        const id = generateUniqueID();
 
         await connection('ongs').insert({
             id,
